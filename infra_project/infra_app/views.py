3e6a4f174ed80ca0c1c020d397442659986ff5a5
neukuhren <exclusive.miha@gmail.com>
2022-08-18 15:13:38 +0300

    retry push

--- infra_project/infra_app/views.py
+++ infra_project/infra_app/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('У меня получилось!')
+    return HttpResponse('У меня получилось задеплоить на я.облако')
 
 
 def second_page(request):
